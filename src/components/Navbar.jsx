dd9f6e3618d8e077d26bddd909c5b4d9384a2bbe
Kushal Rijal <kushal.terhathum.kr7@gmail.com>
2024-05-24 21:11:27 +0545

    menu added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,8 @@ import { navLinks } from '../constants';
 import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
-  const [active, setActive] = useState('')
+  const [active, setActive] = useState('');
+  const [toggle, setToggle] = useState(false);
   return (
     <nav
     className={`${styles.paddindX} w-full flex
@@ -27,7 +28,42 @@ const Navbar = () => {
     >
     <img src ={logo} alt='logo' className='w-9
     h-9 object-contain' />
+    <p className='text-white text-[18px]
+    font-bold cursor-pointer'>Kushal Rijal
+    <span className='sm:block hidden'> | Full Stack Developer</span></p>
     </Link>
+    <ul className='list-none hidden sm:flex flex-row gap-10'>
+      {navLinks.map((Link)=>(
+        <li
+        key={Link.id}
+        className={`${
+          active === Link.title
+            ? "text-white"
+            : "text-secondary"
+        } hover:text-white text-[18px]
+        font-medium cursor-pointer`}
+        onClick={()=> setActive(Link.title)}
+        >
+          <a href={`#${Link.id}`}>{Link.title}</a>
+        </li>
+      ))}
+    </ul>
+    <div className='sm:hidden flex flex-1
+    justify-end items-center'>
+    <img
+    src={toggle ? close : menu}
+    alt = "menu"
+    className='w-[28px] h-[28px]
+    onject-contain cursor-pointer'
+    onClick={() => setToggle(!toggle)}
+     />
+    <div className={`${!toggle ? 'hidden' 
+    :'flex' } p-6 black-gradient absolute
+    top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+
+    </div>
+
+    </div>
     </div>
     </nav>
   )
